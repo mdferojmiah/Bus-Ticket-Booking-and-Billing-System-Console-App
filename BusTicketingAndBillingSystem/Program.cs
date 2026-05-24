4f290309d616b481c5ec0ce18bf81d5cebe50cdc
Feroj Miah <01ferojmiah@gmail.com>
2026-05-24 21:44:55 +0600

    [new] Basic structure has been set.

---
+++ BusTicketingAndBillingSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BusTicketingAndBillingSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Bus Ticket Booking & Billing System");
+            Console.WriteLine("-----------------------------------");
+
+            while (true)
+            {
+                Console.WriteLine("0. Exit\n");
+                Console.Write("> ");
+                var userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case "0":
+                        Console.Clear();
+                        return;
+                }
+            }
+        }
+    }
+}
